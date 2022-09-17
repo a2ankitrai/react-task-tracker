b2b018a19c07d4e4fc47d7c711ba095a5eff80c5
Ankit Rai <a2.ankitrai@gmail.com>
2022-09-17 19:26:11 +0200

    backend api calls

--- src/App.js
+++ src/App.js
@@ -1,72 +1,100 @@
 import { useEffect, useState } from "react";
 import AddTask from "./components/AddTask";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Tasks from "./components/Tasks";
+import About from "./components/About";
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false);
 
-  const [tasks, setTasks] = useState([
-    // {
-    //   id: 1,
-    //   text: "Plan vacation days",
-    //   day: "14 Oct 2022",
-    //   reminder: true
-    // },
-    // {
-    //   id: 2,
-    //   text: "Complete ds problems",
-    //   day: "15 Jul 2023",
-    //   reminder: true
-    // },
-    // {
-    //   id: 3,
-    //   text: "Increase insurance coverage",
-    //   day: "01 Nov 2022",
-    //   reminder: false
-    // }
-  ]);
-
-  const fetchTasks = async () => {
-    const res = await fetch("http://localhost:5000/tasks");
-    const data = await res.json();
-
-    return data;
-  };
+  const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    }
+    getTasks();
+  }, [])
+
+  // Fetch all the tasks
+  const fetchTasks = async () => {
+    const response = await fetch("http://localhost:5000/tasks");
+    const data = await response.json();
+    console.log(data);
+
+    return data;
+  }
+
+  // Fetch specfic tasks
+  const fetchTask = async (id) => {
+    const response = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await response.json();
+    console.log(data);
+
+    return data;
+  }
 
   // Add task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1;
-    const newTask = { id, ...task };
+  const addTask = async (task) => {
 
-    console.log("task add", newTask);
-    setTasks([...tasks, newTask]);
+
+    console.log('task details', task);
+    const res = await fetch(`http://localhost:5000/tasks/`, {
+      method: "POST",
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(task),
+    })
+
+    const data = await res.json();
+    console.log(data);
+    setTasks([...tasks, data]);
+
+    // const id = Math.floor(Math.random() * 10000) + 1;
+    // const newTask = { id, ...task };
+    // console.log("task add", newTask);
+    // setTasks([...tasks, newTask]);
   };
+
+
 
   //Delete tasks
 
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
     console.log("deleting..", { id });
 
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "DELETE",
+    })
+    
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
   // Toggle Reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+
+    const taskToToggle = await fetchTask(id);
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder}
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PUT",
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(updTask),
+    })
+
+    const data = await res.json();
+
     setTasks(
       tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
+        task.id === id ? { ...task, reminder: data.reminder } : task
       )
     );
   };
 
   // const name = 'Ankit';
   return (
-    <div className="App container">
+     <div className="App container">
       <Header title="My todos.." onAdd={() => setShowAddTask(!showAddTask)} />
       {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
@@ -74,7 +102,10 @@ function App() {
       ) : (
         "No Task found!"
       )}
+      <Footer />
+      <About/>
     </div>
+
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>Copyright &copy; Ankit Rai</p>
+    </footer>
+  )
+}
+
+export default Footer
